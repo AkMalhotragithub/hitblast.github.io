962e58246099dc591c424ee5716d82a0b46eddb1
HitBlast <hitblastlive@gmail.com>
2022-07-12 20:13:43 +0600

    Added static files.

---
+++ assets/js/main.js
@@ -0,0 +1,120 @@
+/* SHOW MENU */
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+
+/* ACTIVATE AND REMOVE MENU */
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+/* TYPING CAROUSEL ====*/
+class TxtRotate {
+    constructor(el, toRotate, period) {
+        this.toRotate = toRotate
+        this.el = el
+        this.loopNum = 0
+        this.period = parseInt(period, 10) || 2000
+        this.txt = ''
+        this.tick()
+        this.isDeleting = false
+    }
+    
+    tick() {
+        var i = this.loopNum % this.toRotate.length
+        var fullTxt = this.toRotate[i]
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1)
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1)
+        }
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>'
+
+        var that = this
+        var delta = 300 - Math.random() * 100
+
+        if (this.isDeleting) { delta /= 2} 
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period
+            this.isDeleting = true
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false
+            this.loopNum++
+            delta = 500
+        }
+
+        setTimeout(function () {
+            that.tick()
+        }, delta)
+    }
+}
+
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+
+    var css = document.createElement("style");
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.05em solid #859398; }";
+    document.body.appendChild(css);
+};
+
+
+/* SCROLL REVEAL ANIMATION */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '40px',
+    duration: 1500,
+    reset: false
+});
+
+/*SCROLL: HOME*/
+sr.reveal('.home__title', {});
+sr.reveal('.button', {delay: 200});
+sr.reveal('.home__img', {delay: 400});
+sr.reveal('.home__social-icon', {interval: 190});
+
+/*SCROLL: ME*/
+sr.reveal('.me__img', {});
+sr.reveal('.me__subtitle', {delay: 400});
+sr.reveal('.me__text', {delay: 400});
+
+/*SCROLL: SKILLS*/
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__text', {});
+sr.reveal('.skills__data', {interval: 190});
+sr.reveal('.skills__img', {delay: 600});
+
+/*SCROLL: PROJECTS*/
+sr.reveal('.projects__img', {interval: 190});
+
+/*SCROLL: CERTIFICATIONS*/
+sr.reveal('.certs__img', {interval: 190});
